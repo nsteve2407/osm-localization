23e07f66bb0be7e74dcb7eeb947d0254536e3702
Stephen Ninan <nstephen@tamu.edu>
2021-11-19 16:34:01 -0600

    Added functions to osm_pf class

--- include/osm_pf.h
+++ include/osm_pf.h
@@ -15,12 +15,13 @@ namespace osmpf
 {
     class osm_pf
     {
+        typedef _Float64 f;
         private:
-        xt::xarray<_Float64> d_matrix;
-        _Float64 origin_x;
-        _Float64 origin_y;
-        _Float64 max_x;
-        _Float64 max_y;
+        xt::xarray<f> d_matrix;
+        f origin_x;
+        f origin_y;
+        f max_x;
+        f max_y;
         ros::NodeHandle nh;
         ros::Publisher pf_publisher;
         message_filters::Subscriber<nav_msgs::Odometry> odom_sub;
@@ -28,10 +29,17 @@ namespace osmpf
         typedef message_filters::sync_policies::ApproximateTime<nav_msgs::Odometry,sensor_msgs::PointCloud2> sync_policy;
         message_filters::Synchronizer<sync_policy> sync;
         int num_particles;
-        xt::xarray<_Float64> X_t;
-        xt::xarray<_Float64> W_t;
+        xt::xarray<f> Xt;
+        xt::xarray<f> Wt;
         std::string d_mat_path;
         public:
-
+        osm_pf(std::string path_to_d_mat);
+        xt::xarray<f> sample_pose();
+        f find_xbar(f x_tminus1,nav_msgs::Odometry odom);
+        xt::xarray<f> find_Xbar(xt::xarray<f> X_tminus1,nav_msgs::Odometry odom);
+        f find_wt(f xbar,sensor_msgs::PointCloud2 p_cloud);
+        f find_wt_x(float point);
+        xt::xarray<f> find_Wt(xt::xarray<f> Xtbar,sensor_msgs::PointCloud2 p_cloud);
+        xt::xarray<f> sample_xt(xt::xarray<f> Xbar_t,xt::xarray<f> Wt);
     };
 }
