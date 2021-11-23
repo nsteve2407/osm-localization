0428d9f225fc48e12c57f96c6022eca7cce8ed96
Stephen Ninan <nstephen@tamu.edu>
2021-11-22 19:03:27 -0600

    Added X_bar functionality

--- include/osm_pf.h
+++ include/osm_pf.h
@@ -38,9 +38,11 @@ namespace osmpf
         typedef message_filters::sync_policies::ApproximateTime<nav_msgs::Odometry,sensor_msgs::PointCloud2> sync_policy;
         message_filters::Synchronizer<sync_policy>* sync;
         int num_particles;
-        std::shared_ptr<std::vector<pose>> Xt;
-        std::shared_ptr<std::vector<f>> Wt;
-    
+        // std::shared_ptr<std::vector<pose>> Xt;
+        // std::shared_ptr<std::vector<f>> Wt;
+        std::vector<pose> Xt;
+        std::vector<f> Wt;
+        f cov_lin, cov_angular;
         public:
         osm_pf(std::string path_to_d_mat,f min_x,f min_y,f Max_x,f Max_y,int particles=100);
         void init_particles();
--- src/osm_pf.cpp
+++ src/osm_pf.cpp
@@ -1,6 +1,7 @@
 #include "osm_pf.h"
 #include<random>
 #include<math.h>
+#include<tf/transform_datatypes.h>
 
 using namespace osmpf;
 
@@ -16,9 +17,12 @@ osm_pf::osm_pf(std::string path_to_d_mat,f min_x,f min_y,f Max_x,f Max_y,int par
     odom_sub.subscribe(nh,"odometry_topic",100);
     pc_sub.subscribe(nh,"os_cloud_node/points",100);
     sync = new message_filters::Synchronizer<sync_policy>(sync_policy(10),odom_sub,pc_sub);
-    
-    Xt = std::make_shared<std::vector<pose>>();
-    Wt = std::make_shared<std::vector<f>>();
+    cov_lin = (f)25;
+    cov_angular = (f)M_PI_2;
+    // Xt = std::make_shared<std::vector<pose>>();
+    // Wt = std::make_shared<std::vector<f>>();
+    Xt = std::vector<pose>(num_particles);
+    Wt = std::vector<f>(num_particles);
 }
 
 void osm_pf::init_particles()
@@ -34,6 +38,31 @@ void osm_pf::init_particles()
         f _y = distribution_y(generator);
         f _theta = distribution_theta(generator);
         pose p(_x,_y,_theta);
-        Xt->emplace_back(p);
+        Xt[i]=p;
     }    
+}
+
+pose osm_pf::find_xbar(pose x_tminus1,nav_msgs::Odometry odom)
+{
+    f dx = odom.pose.pose.position.x;
+    f dy = odom.pose.pose.position.y;
+    f dtheta =  tf::getYaw(odom.pose.pose.orientation); //check sign converntions
+
+    std::default_random_engine generator;
+    std::normal_distribution<f> dist_x(x_tminus1.x+dx,cov_lin);
+    std::normal_distribution<f> dist_y(x_tminus1.y+dy,cov_lin);
+    std::normal_distribution<f> dist_theta(x_tminus1.theta+dtheta,cov_angular);
+    pose p_k_bar(dist_x(generator),dist_y(generator),dist_theta(generator));
+    return p_k_bar;
+}
+
+std::vector<pose> osm_pf::find_Xbar(std::vector<pose> X_tminus1,nav_msgs::Odometry odom)
+{
+    std::vector<pose> Xt_bar(num_particles);
+    for(int i=0;i<num_particles;i++)
+    {
+        Xt_bar[i] = find_xbar(X_tminus1[i],odom);
+    }
+    return Xt_bar;
+
 }
