6194c1e7c8641b925627ef8123670b8b2d0c73ef
Stephen Ninan <nstephen@tamu.edu>
2021-11-23 19:49:13 -0600

    Defined  main function

--- src/pf_node.cpp
+++ src/pf_node.cpp
@@ -5,39 +5,42 @@
 #include <xtensor.hpp>
 #include <xtensor/xnpy.hpp>
 #include <iostream>
+#include "include/osm_pf.h"
 
 
-
-
-
-
-
-
-
-
-
-
+typedef _Float64 f;
 
 
 int main(int argc,char** argv)
 {
     ros::init(argc,argv,"Particle_Filtrer");
-    ros::NodeHandle ng;
+    ros::NodeHandle nh;
 
     // Values to access
+    std::string path;
+    f min_x,min_y,max_x,max_y;
+    nh.getParam("path_to_dmat",path);
+    nh.getParam("min_x",min_x);
+    nh.getParam("min_y",min_y);
+    nh.getParam("max_x",max_x);
+    nh.getParam("max_y",max_y);
+    osmpf::osm_pf particle_filter(path,min_x,min_y,max_x,max_y,500);
 
-    // Arrayed saved in   n,e   /  lat,lon / y,x
-    int n = 0;
-    int e = 979;
+    // Run Particle Filter
+    particle_filter.run();
 
-    // cnpy::NpyArray d_mat = cnpy::npy_load("a.npy");
+    // // Arrayed saved in   n,e   /  lat,lon / y,x
+    // int n = 0;
+    // int e = 979;
+
+    // // cnpy::NpyArray d_mat = cnpy::npy_load("a.npy");
     
-    auto d_grid = xt::load_npy<_Float64>("/home/mkz/catkin_ws/src/osm-localization/distance_maps/d_grid.npy");
-    // xt::view(d_grid,xt::all(),2);
-    auto distance = d_grid(e,n);
-    std::cout<<distance<<std::endl;
-    // auto c= d_grid(0,0)
-    // printf(d_grid(0,0));
+    // auto d_grid = xt::load_npy<_Float64>("/home/mkz/catkin_ws/src/osm-localization/distance_maps/d_grid.npy");
+    // // xt::view(d_grid,xt::all(),2);
+    // auto distance = d_grid(e,n);
+    // std::cout<<distance<<std::endl;
+    // // auto c= d_grid(0,0)
+    // // printf(d_grid(0,0));
 
     return 0;
     
